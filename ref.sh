76a8b3a7443bde70ded479d411aaae732bef9adc
732047249 <jiaolifeng@yahoo.com>
2020-01-01 18:04:22 +0800

    Update ref.sh

--- ref.sh
+++ ref.sh
@@ -5,3 +5,4 @@ docker-compose scale kafka=4
 docker ps
 docker exec kafka-docker_kafka_1 kafka-topics.sh --create --topic topic001 --partitions 4 --zookeeper zookeeper:2181 --replication-factor 2
 docker exec kafka-docker_kafka_3 kafka-topics.sh --list --zookeeper zookeeper:2181 topic001
+docker exec kafka-docker_kafka_3   kafka-topics.sh   --describe  --topic topic001  --zookeeper zookeeper:2181
