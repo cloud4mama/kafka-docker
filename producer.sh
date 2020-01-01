c634e67b47c5c60e1177eeb015459399426de55b
732047249 <jiaolifeng@yahoo.com>
2020-01-01 17:58:00 +0800

    Create producer.sh

---
+++ producer.sh
@@ -0,0 +1,1 @@
+docker exec -it kafka-docker_kafka_1 kafka-console-producer.sh --topic topic001  --broker-list kafka-docker_kafka_1:9092,kafka-docker_kafka_2:9092,kafka-docker_kafka_3:9092,kafka-docker_kafka_4:9092
