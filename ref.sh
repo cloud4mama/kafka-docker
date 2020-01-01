dc4595f58ff6c0c74d05e7004aedd7d1dee8e632
732047249 <jiaolifeng@yahoo.com>
2020-01-01 18:01:16 +0800

    Create ref.sh

---
+++ ref.sh
@@ -0,0 +1,7 @@
+# https://blog.csdn.net/boling_cavalry/article/details/85395080
+
+docker-compose up -d
+docker-compose scale kafka=4
+docker ps
+docker exec kafka-docker_kafka_1 kafka-topics.sh --create --topic topic001 --partitions 4 --zookeeper zookeeper:2181 --replication-factor 2
+docker exec kafka-docker_kafka_3 kafka-topics.sh --list --zookeeper zookeeper:2181 topic001
