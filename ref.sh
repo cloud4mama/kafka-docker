eb1cee0df30b53046865cd8d388feedd7d230264
732047249 <jiaolifeng@yahoo.com>
2020-01-01 18:15:07 +0800

    Update ref.sh

--- ref.sh
+++ ref.sh
@@ -1,6 +1,8 @@
 # https://blog.csdn.net/boling_cavalry/article/details/85395080
 
 docker-compose up -d
+docker exec kafka-docker_kafka_1 find / -name \*kafka_\* | head -1 | grep -o '\kafka[^\n]*'
+docker exec kafka-docker_zookeeper_1 pwd
 docker-compose scale kafka=4
 docker ps
 docker exec kafka-docker_kafka_1 kafka-topics.sh --create --topic topic001 --partitions 4 --zookeeper zookeeper:2181 --replication-factor 2
