39c032bda9fa2ce195f2c5fd77341ffc5309578d
732047249 <jiaolifeng@yahoo.com>
2020-01-01 18:08:11 +0800

    Create install-docker-compose.sh

---
+++ install-docker-compose.sh
@@ -0,0 +1,2 @@
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
